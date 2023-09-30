cd5c28a2761ea56484d310df4cf5730a67191815
callacat <wang892947707@hotmail.com>
2023-09-30 23:59:39 +0800

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,15 +3,6 @@
 # 默认定时
 time='0 5 * * *'
 
-# 如果存在自定义的cron.sh，执行它
-if [ -f /config/cron.sh ]; then
-    chmod +x /config/cron.sh
-    /config/cron.sh
-else
-    # 设置时间
-    time=${CRON_TIME:-'0 5 * * *'}
-fi
-
 # 如果 /data/config.conf 不存在，将其移动到 /data 目录
 if [ ! -f /data/config.conf ]; then
     mv /app/config.conf /data/config.conf
@@ -27,17 +18,26 @@ if [ ! -L /app/config.conf ]; then
     ln -s /data/config.conf /app/config.conf
 fi
 
+# 如果存在自定义的cron.sh，执行它
+if [ -f /data/cron.sh ]; then
+    chmod +x /data/cron.sh
+    /data/cron.sh
+else
+    # 设置时间
+    time=${CRON_TIME:-'0 5 * * *'}
+fi
+
 # 根据 ENABLE_DOWNLOAD 变量选择要执行的命令
 if [ "$ENABLE_DOWNLOAD" = "true" ]; then
     echo "将使用优选IP进行测速"
-    command="/app/yxip.sh"
+    cron_command="/app/yxip.sh"
 else
     echo "未选择优选IP进行测速，使用默认IP"
-    command="/app/start.sh"
+    cron_command="/app/start.sh"
 fi
 
 # 创建 cron 作业
-echo "$time $command >> /var/log/cron.log 2>&1" > /etc/cron.d/cf_ddns-cron
+echo "$time \"$cron_command >> /var/log/cron.log 2>&1\"" > /etc/cron.d/cf_ddns-cron
 
 # 载入 cron 作业
 crontab /etc/cron.d/cf_ddns-cron
