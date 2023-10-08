e116ea30bb23d0b237c08ec5baf1de921f416dab
callacat <wang892947707@hotmail.com>
2023-10-09 00:40:56 +0800

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -12,14 +12,13 @@ cp /app/backup/config.conf.bak /data/config.bak
 
 # 判断是否存在配置文件
 if [ ! -f /data/config.conf ]; then
- # 如果不存在则输出提示并创建配置文件然后退出
+  # 如果不存在则输出提示并创建配置文件然后退出
   echo "首次启动请编辑/data映射目录下配置文件后重启容器"
   cp /app/backup/config.conf.bak /data/config.conf
   exit 1 # 初始化配置后退出脚本
 else
-  # 如果存在则在每次启动时强制创建软链接
+  # 如果存在则在每次启动时检查并创建软链接
   if [ ! -L /app/config.conf ]; then
-    rm /app/config.conf
     ln -sf /data/config.conf /app/config.conf
   fi
   /app/start.sh
