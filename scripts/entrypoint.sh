ffc2cdf4cfc873bdb27d4b6244baf9b9a6bb87c9
callacat <wang892947707@hotmail.com>
2023-10-10 20:45:02 +0800

    回退

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+<<<<<<< HEAD
 # 每次启动时显示镜像打包时间
 BUILD_TIME=$(cat /app/creat.txt)
 echo -e "\033[32m镜像打包时间:$BUILD_TIME\033[0m"
@@ -13,3 +14,56 @@ if [ ! -f /data/config.conf ]; then
 else
   /data/start.sh
 fi
+    else # 其他情况使用默认ip
+      log_start  
+>>>>>>> parent of 0305f5c (优化显示)
+=======
+      log_start  # 日志记录
+      cp /data/ip.txt /app/cf_ddns/ip.txt # 拷贝自定义ip文件
+      set_cron "/app/start.sh" # 设置定时任务
+    elif [ "$IP_PR_IP" = "true" ]; then # 开启IP_PR模式
+      log_start
+      set_cron "/app/start.sh"
+    else # 其他情况使用默认ip
+      log_start  
+>>>>>>> parent of 0305f5c (优化显示)
+      cp /app/ip.txt /app/cf_ddns/ip.txt # 拷贝默认ip文件
+      set_cron "/app/start.sh"
+    fi
+    ;;
+esac
+
+# 执行一次性任务函数
+run_once() {
+<<<<<<< HEAD
+<<<<<<< HEAD
+  cd /app && $cron_command >> $LOG_FILE 2>&1 &
+=======
+=======
+>>>>>>> parent of 0305f5c (优化显示)
+#  log_start
+  cd /app && $cron_command >> /tmp/cron.log 2>&1 &
+>>>>>>> parent of 0305f5c (优化显示)
+}
+
+echo "$cron_command"
+echo "$IP_PR_IP"
+echo "$ENABLE_DOWNLOAD"
+echo "$CRON_TIME"
+
+# 启动时执行一次测速任务
+run_custom && run_once
+
+# 输出定时任务日志
+<<<<<<< HEAD
+<<<<<<< HEAD
+tail -f $LOG_FILE
+=======
+echo -e "\033[32m已加入定时任务，当前定时: $CRON_TIME\033[0m\n"
+tail -f /tmp/cron.log
+>>>>>>> parent of 0305f5c (优化显示)
+=======
+echo -e "\033[32m已加入定时任务，当前定时: $CRON_TIME\033[0m\n"
+tail -f /tmp/cron.log
+>>>>>>> parent of 0305f5c (优化显示)
+>>>>>>> parent of 4b3c30c (撤销更改)
---
+++ scripts/yxip.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# 创建临时目录并下载txt.zip
+mkdir -p tmp && wget -4 -q -c --timeout=15 --header="User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36 Edg/117.0.2045.43" -O tmp/txt.zip https://zip.baipiao.eu.org
+
+# 判断下载是否成功
+if [ $? -eq 0 ]; then
+    echo -e "\033[31m下载优选IP完成，开始测速\033[0m\n"
+    rm -f /app/cf_ddns/ip.txt # 删除旧的ip.txt文件
+    unzip -qo tmp/txt.zip -d tmp # 解压缩txt.zip文件到临时目录
+    cat tmp/*.txt | sort | uniq > /app/cf_ddns/ip.txt # 将所有txt文件合并到一个ip.txt文件中，并去重排序
+else
+    echo -e "\033[31m下载优选IP失败，使用默认IP\033[0m\n"
+    rm -f /app/cf_ddns/ip.txt # 删除旧的ip.txt文件
+    cp /app/ip.txt /app/cf_ddns/ip.txt # 复制默认的ip.txt文件
+fi
+
+# 删除临时目录
+rm -rf tmp
+
+# 执行/app/start.sh
+exec /app/start.sh
