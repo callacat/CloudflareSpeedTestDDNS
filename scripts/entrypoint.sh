a4b9945b731aa0841ba6973220ae2e737b77d170
callacat <wang892947707@hotmail.com>
2023-10-18 22:02:05 +0800

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -16,62 +16,69 @@ else
 
   # 加载配置文件
   source /app/config.conf
-  source /data/cron.conf
 
   # 定义日志函数,显示当前执行时间
   log_start() {
     /app/time.sh
   }
 
-  # 判断配置走不同逻辑
-  case "$ENABLE_DOWNLOAD" in
+  # 执行一次性任务函数
+  run_once() {
+    cd /app && $cron_command >> /tmp/cron.log 2>&1 &
+  }
 
-  # 如果启用下载  
-    true)
-      log_start # 日志记录 
-      echo "当前使用优选IP进行测速"
-      set_cron "/app/yxip.sh" # 设置定时任务 
-      ;;
+  # 如果cron.conf存在
+  if [ -f /data/cron.conf ]; then
+    # 加载cron.conf
+    source /data/cron.conf
 
-  # 如果不启用  
-    false)
-      if [ -f /data/ip.txt ]; then # 自定义ip文件存在
-        log_start  # 日志记录
-        echo "当前使用自定义IP进行测速"
-        cp /data/ip.txt /app/cf_ddns/ip.txt # 拷贝自定义ip文件
-        set_cron "/app/start.sh" # 设置定时任务
-      elif [ "$IP_PR_IP" = "true" ]; then # 开启IP_PR模式
-        log_start
-        echo "当前使用IP_PR模式进行测速"
-        set_cron "/app/start.sh"
-      else # 其他情况使用默认ip
-        log_start
-        echo "当前使用默认IP进行测速"
-        cp /app/ip.txt /app/cf_ddns/ip.txt # 拷贝默认ip文件
-        set_cron "/app/start.sh"
-      fi
-      ;;
-  esac
+    # 判断配置走不同逻辑
+    case "$ENABLE_DOWNLOAD" in
 
-  # 如果定时任务不为空
-  if "$CRON_TIME" != "" ; then
-    # 创建定时任务函数 
-    set_cron() {
-      cron_command=$1 # 获取参数作为要运行的命令
-      echo "$CRON_TIME cd /app && /app/time.sh && $cron_command >> /tmp/cron.log 2>&1" > /etc/crontabs/cfyx # 写入定时任务
-      crontab /etc/crontabs/cfyx && crond & # 载入定时任务并在后台运行
-    }
+    # 如果启用下载  
+      true)
+        log_start # 日志记录 
+        echo "当前使用优选IP进行测速"
+        set_cron "/app/yxip.sh" # 设置定时任务 
+        ;;
 
-    # 输出定时任务日志
-    tail -f /tmp/cron.log
+    # 如果不启用  
+      false)
+        if [ -f /data/ip.txt ]; then # 自定义ip文件存在
+          log_start  # 日志记录
+          echo "当前使用自定义IP进行测速"
+          cp /data/ip.txt /app/cf_ddns/ip.txt # 拷贝自定义ip文件
+          set_cron "/app/start.sh" # 设置定时任务
+        elif [ "$IP_PR_IP" = "true" ]; then # 开启IP_PR模式
+          log_start
+          echo "当前使用IP_PR模式进行测速"
+          set_cron "/app/start.sh"
+        else # 其他情况使用默认ip
+          log_start
+          echo "当前使用默认IP进行测速"
+          cp /app/ip.txt /app/cf_ddns/ip.txt # 拷贝默认ip文件
+          set_cron "/app/start.sh"
+        fi
+        ;;
+    esac
 
+    # 如果定时任务不为空
+    if "$CRON_TIME" != "" ; then
+      # 创建定时任务函数 
+      set_cron() {
+        cron_command=$1 # 获取参数作为要运行的命令
+        echo "$CRON_TIME cd /app && /app/time.sh && $cron_command >> /tmp/cron.log 2>&1" > /etc/crontabs/cfyx # 写入定时任务
+        crontab /etc/crontabs/cfyx && crond & # 载入定时任务并在后台运行
+      }
+
+      # 输出定时任务日志
+      tail -f /tmp/cron.log
+    else
+      # 启动时执行一次测速任务
+      run_once
+    fi
   else
-    # 执行一次性任务函数
-    run_once() {
-      cd /app && $cron_command >> /tmp/cron.log 2>&1 &
-    }
-
-    # 启动时执行一次测速任务
-    run_once
+    # 直接启动脚本
+    /app/start.sh
   fi
 fi
